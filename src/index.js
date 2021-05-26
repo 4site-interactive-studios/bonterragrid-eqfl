2675d23c16cf353bcff061f5735e80c8de3530fa
Martin Kenjic <martin.kenjic@agiledrop.com>
2021-05-26 16:02:05 +0200

    Add background, content sections, Add Accordion

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 //import css from "./main.css";
 import scss from "./sass/main.scss";
+
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
