c429a30546a0aeb3b9f50b16aff0324994b787eb
Martin Kenjic <martin.kenjic@agiledrop.com>
2021-06-01 14:08:07 +0200

    Add take action button on mobile, Add becomeMember to form, Add footer, Add sections styling, Fix responsive, Add KIRO font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //import css from "./main.css";
 import scss from "./sass/main.scss";
 
+/**
+ * Display Accordion panel
+ */
 let acc = document.getElementsByClassName("accordion");
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -15,6 +18,9 @@ for (let i = 0; i < acc.length; i++) {
   });
 }
 
+/**
+ * Contribution Information - Select amount
+ */
 const labelAmount = document.querySelectorAll('.label-amount');
 const labelOtheramount = document.querySelector('.label-otheramount input[type="number"]');
 
@@ -43,9 +49,26 @@ labelAmount.forEach(label => {
 });
 
 /**
- * Move elements from selected frequency radios
+ * Add become member element to form
  */
+const contributionInformation = document.querySelector(".ContributionInformation");
 
+function addBecomeMember() {
+  const becomeMember = `
+  <div class="becomeMember">
+    <img src="imgs/cc.png" border="0" alt="" title="" />
+    <div class="becomeMember-content">
+      <h2>Become a member!</h2>
+      <p>With a one-time contribution of $35 or more, members receive an Equality Florda Membership Card and enamel pin.</p>
+    </div>
+  </div>
+  `;
+  contributionInformation.insertAdjacentHTML('afterend', becomeMember);
+}
+
+/**
+ * Move elements from selected frequency radios
+*/
 const selectedFrequency = document.querySelector(".form-item-selectedfrequency");
 const radios = selectedFrequency.querySelector(".radios");
 const radioDescription = document.querySelector(".radio-description");
@@ -68,27 +91,67 @@ function moveRadiosElements() {
   radios.insertAdjacentHTML('beforeend', '<div class="radios__indicator"></div>');
 }
 
+/** 
+ * Run functions
+*/
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', moveRadiosElements);
+  isViewport();
+  addBecomeMember();
 } else {
   moveRadiosElements();
+  isViewport();
+  addBecomeMember();
 }
-
 
 /**
  * Add checkmark to checkboxes
  */
 const checkboxes = document.querySelectorAll('label.at-check');
+
 checkboxes.forEach(checkbox => {
   checkbox.insertAdjacentHTML('beforeend', '<div class="checkmark"></div>');
 })
 
-
 /**
  * Add checkmark to radio buttons
  */
+
  const radioButtons = document.querySelectorAll("[class^='at-radio']");
+
  radioButtons.forEach(radio => {
    radio.insertAdjacentHTML('beforeend', '<div class="checkmark"></div>');
  })
  
+
+/**
+* Intersection Observer viewport & scroll into view
+*/
+const takeAction = document.querySelector('.take-action');
+
+function isViewport() {
+
+  const target = document.querySelector('.ContributionInformation .radios');
+  const config = { rootMargin: '0px', threshold: 1.0 }
+
+  let callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting || (entry.boundingClientRect.top <= 0)) {
+        takeAction.style.display = "none";
+      } else {
+        takeAction.style.display = "";
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback, config);
+  observer.observe(target);
+
+}
+
+takeAction.addEventListener('click', function (e) {
+  e.preventDefault();
+  document.querySelector('.ContributionInformation').scrollIntoView({
+      behavior: 'smooth'
+  });
+});
